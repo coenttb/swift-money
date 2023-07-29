275968b391f4b2d1b53bc20e58fc7ce5bfd266bb
Coen ten Thije Boonkkamp <coen@legalkit.app>
2023-07-29 15:38:13 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ extension Target.Dependency {
 
 let package = Package(
     name: .money,
+    platforms: [.macOS(.v13), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
